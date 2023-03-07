220e4c5668f2e4a3dc42c32cc037a6c5af7b124c
RedJyu <piotr.stezycki@protonmai.com>
2023-03-07 15:24:22 +0100

    added public and fixed css

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import router from './routes/admin/auth.js';
 import cookieSession from 'cookie-session';
 const app = express();
+
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
